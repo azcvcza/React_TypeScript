02d9c9716ba47bdb9e62a125eced1dba3e858f0e
韦粤信 <283940802@qq.com>
2019-07-03 02:08:46 +0800

    build AdminPage

---
+++ my-react-ts-app/src/components/AdminPage.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+const AdminPage:React.SFC =()=>{
+	return(<div className="page-container">
+		<h1>Admin Panel</h1>
+		<p>you should only to be here if logged in</p>
+	</div>)
+}
+export default AdminPage
