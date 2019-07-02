b1b826c73bf683e1b3ce45a6b7eabe0a7bfd647d
韦粤信 <283940802@qq.com>
2019-07-03 02:03:24 +0800

    complete ProductPage

--- my-react-ts-app/src/components/ProductsPage.tsx
+++ my-react-ts-app/src/components/ProductsPage.tsx
@@ -1,16 +1,31 @@
-import * as React  from 'react';
-import {IProduct,products} from '../router/ProductsData';
+import * as React from 'react';
+import { IProduct, products } from '../router/ProductsData';
 
-interface IState{
-	products:IProduct[];
+interface IState {
+	products: IProduct[];
 }
 
-class ProductsPage extends React.Component<{},IState>{
-	public constructor(props:{}){
+class ProductsPage extends React.Component<{}, IState>{
+	public constructor(props: {}) {
 		super(props);
-		this.state={
-			products:[]
+		this.state = {
+			products: []
 		}
+	}
+	public componentDidMount() {
+		this.setState({ products })
+	}
+	public render() {
+		return (<div className="page-control">
+			<p>Welcome to React Router</p>
+			<ul className="product-list">
+				{this.state.products.map(product => (
+					<li key={product.id} className="product-list-item">
+						{product.name}
+					</li>
+				))}
+			</ul>
+		</div>)
 	}
 }
 
